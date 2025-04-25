63ca0e58372da4a09a9d2d08f08400e2bf57ad49
chetandvsharma <thechetan009@gmail.com>
2025-04-26 01:47:37 +0530

    filter and sorting

--- src/services/queryFromPinecone.js
+++ src/services/queryFromPinecone.js
@@ -2,12 +2,22 @@ import pinecone from "../lib/pineconeClient.js";
 
 const index = pinecone.index("ai-memory");
 
-export async function queryPinecone(embedding, topK = 5) {
+export async function queryPinecone(embedding, source = false, topK = 5) {
   try {
     const result = await index.query({
       vector: embedding,
-      topK,
+      topK: 100, // response limit  (same as mongodb limit )
       includeMetadata: true,
+      // filter: {
+      //   source: source ? source : "user-input",
+      //   // source: "tip", 
+      //   // source: "chat"
+      // },
+      // ⏳ filter by timestamp range
+      // timestamp: {
+      //   $gte: "2025-04-23T00:00:00.000Z",
+      //   $lte: "2025-04-25T00:00:00.000Z",
+      // },
     });
 
     console.log("🔍 Query result:", JSON.stringify(result, null, 2));
