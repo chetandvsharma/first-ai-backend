5a5a2e1816f5bd91bdcf513553b2b0d3bf7e7466
chetandvsharma <thechetan009@gmail.com>
2025-04-27 19:42:36 +0530

    ai retrival routing and controller

--- src/routes/memoryRoutes.js
+++ src/routes/memoryRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { storeMemory,queryMemory } from "../controllers/memoryController.js";
+import {
+  storeMemory,
+  queryMemory,
+  aiRetrival,
+} from "../controllers/memoryController.js";
 
 const router = express.Router();
 
 router.post("/memory", storeMemory);
 router.get("/memory", queryMemory);
+
+router.get("/chets-gpt", aiRetrival);
 
 export default router;
